5c63c8c8706e57e761b4a2d0cf957f0a0232fe3c
Danii44 <danishahmad444@gmail.com>
2026-07-14 15:32:07 +0400

    netlify

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      // Rewrite any app route (except Next internals and static files) to the dark folder.
-      {
-        // only match non-empty paths (avoid rewriting the root '/')
-        source: '/:path((?!_next|api|static|assets|favicon.ico).+)',
-        destination: '/dark/:path',
-      },
-    ];
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
   },
 };
 
